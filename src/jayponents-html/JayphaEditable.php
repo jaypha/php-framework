98e65628baded2626eb8c0b5fb6c2a8bae78bf8f
Jason den Dulk <jason@jaypha.com.au>
2019-08-30 13:05:57 +1000

    Added Jaypha web components

---
+++ src/jayponents-html/JayphaEditable.php
@@ -0,0 +1,29 @@
+<?php
+//----------------------------------------------------------------------------
+//
+//----------------------------------------------------------------------------
+
+namespace Jaypha\Jayponents\Html;
+
+class JayphaEditable extends Control
+{
+  public $value = null;
+
+  function __construct($name, $form=null)
+  {
+    parent::__construct($name);
+    $this->tagName = "jaypha-editable";
+  }
+
+  function display()
+  {
+    $this->add($this->value);
+    parent::display();
+  }
+}
+
+//----------------------------------------------------------------------------
+// Copyright (C) 2018 Jaypha.
+// License: BSL-1.0
+// Author: Jason den Dulk
+//
